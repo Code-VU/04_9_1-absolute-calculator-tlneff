4d2d623aece2b420269da4311753e1ab980289fa
tlneff <neff.tyler@gmail.com>
2024-04-19 19:24:37 -0500

    added program logic

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,10 +1,21 @@
 def calculateAbsolute():
-    
+
     # This first line is provided for you
-    in_num  = input("Enter a number: ")
+    in_num = input("Enter a number: ")
+    try:
+        in_num = int(in_num)
+    except:
+        print("Not valid!")
+        quit()
+
+    result = abs(21 - in_num)
+    if in_num > 21:
+        result = result * 2
+    print("Result:", result)
     # end assignment
 
-## If you want to test locally run > python payCalculator.py
+# If you want to test locally run > python payCalculator.py
+
 
 if __name__ == "__main__":
     calculateAbsolute()
